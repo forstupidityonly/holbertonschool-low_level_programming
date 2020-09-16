f78bd4cfcd30cf05641b7535f966bb22eef007d6
Corbin <forstupidityonly@gmail.com>
2020-09-16 14:52:52 -0500

    spell

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
-#include <studio.h>
+#include <stdio.h>
 /**
   * main - program entry point
   * Return: 0
   */
 int main(void)
 {
-	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	rintf("Size of a char: %ld byte(s)\n", sizeof(char));
 	printf("Size of an int: %ld byte(s)\n", sizeof(int));
 	printf("Size of a long int: %ld byte(s)",size of(long));
 	printf("Size of a long long int: %ld byte(s) \n", sizeof(long long));
