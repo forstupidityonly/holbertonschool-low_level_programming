4426c0dfeb87f93606077dbfc9b98d249bae4384
Corbin <forstupidityonly@gmail.com>
2020-09-17 10:57:09 -0500

    no else needed

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  * main - entry point
+  * Return: (0)
+  */
+int main(void)
+{
+	int n;
+	int j;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	j = (n % 10);
+	if (j > 5)
+		printf ("%d and is greater than 5\n", j);
+	else if (j == 0)
+		printf ("%d and is 0\n", j);
+	else if (j < 6 && j != 0)
+		printf ("%d and is less than 6 and not 0\n", j);
+	return (0);
+}
