df15fff937f9aa2daf3667c30d80cebff63ec1e9
Corbin <forstupidityonly@gmail.com>
2020-09-21 10:51:43 -0500

    ascii

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+  * print_alphabet - what u think it do?
+  * Return: nope
+  */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		_putchar(i);
+	_putchar('\n');
+}
