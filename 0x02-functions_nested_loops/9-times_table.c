c52547e5042ea217c3b1071551bbc9c6dbd8a180
Corbin <forstupidityonly@gmail.com>
2020-09-21 17:34:40 -0500

    this sucked

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+  *
+  */
+void times_table(void)
+{
+	int i, j, n;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			n = (i * j);
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (n >= 10)
+			{
+				_putchar((n/10) + '0');
+				_putchar((n%10) + '0');
+			}
+			else if (n < 10 && j != 0)
+			{
+				_putchar(' ');
+				_putchar((n%10) + '0');
+			}
+			else
+				_putchar((n%10) + '0');
+		}
+		_putchar('\n');
+	}
+}
