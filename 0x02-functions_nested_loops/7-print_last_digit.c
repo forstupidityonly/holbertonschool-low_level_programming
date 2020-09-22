a3daf0a5823fd8946c6f70c3181d8490b3d418e5
Corbin <forstupidityonly@gmail.com>
2020-09-22 09:44:11 -0500

    betty

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,13 @@
 #include "holberton.h"
 /**
-  *
+  * print_last_digit - what u think betty?
+  * @a: the int
+  * Return: the int
   */
 int print_last_digit(int a)
 {
 	if (a < 0)
-		a *= -1;
+		a = a * -1;
 	a %= 10;
 	_putchar(a + '0');
 	return (a);
