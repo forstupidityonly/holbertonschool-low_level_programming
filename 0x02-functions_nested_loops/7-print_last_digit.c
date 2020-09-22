40d6810994f807df2c97b4563f7d4e3e611d9f6b
Corbin <forstupidityonly@gmail.com>
2020-09-22 09:51:51 -0500

    if 0

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,12 @@ int print_last_digit(int a)
 {
 	if (a < 0)
 		a = a * -1;
-	a = a % 10;
+	else if (a == 0)
+	{
+		_putchar(a + '0');
+		return (a);
+	}
+	a %= 10;
 	_putchar(a + '0');
 	return (a);
 }
