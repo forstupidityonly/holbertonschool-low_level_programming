e02bd8a984c61b326259bcc249517b0e4145bca1
Corbin <forstupidityonly@gmail.com>
2020-09-24 12:48:10 -0500

    that was dummy hard

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,5 +5,16 @@
   */
 void more_numbers(void)
 {
+	int i, j;
 
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
