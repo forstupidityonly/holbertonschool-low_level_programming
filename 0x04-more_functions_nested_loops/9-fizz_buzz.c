1a719dc389f64e9ebe4012e89f89e1653c7af1e3
Corbin <forstupidityonly@gmail.com>
2020-09-24 15:57:06 -0500

    formatting

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,8 +8,9 @@ int main(void)
 {
 	for (int i = 1; i <= 100; ++i)
 	{
-		if (i % 3 == 0) printf("fizz ");
-		if (i % 5 == 0) printf("buzz ");
+		if (i % 15 == 0) printf("FizzBuzz ");
+		if (i % 3 == 0) printf("Fizz ");
+		if (i % 5 == 0) printf("Buzz ");
 		if (i * i * i * i % 15 == 1) printf("%d ", i);
 	}
 }
