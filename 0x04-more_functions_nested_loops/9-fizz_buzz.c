590901376a76703e1b4ae0a0814717648d0acd0e
Corbin <forstupidityonly@gmail.com>
2020-09-24 16:07:52 -0500

    fine stupid checker

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,11 +5,17 @@
   */
 int main(void)
 {
-	for (int i = 1; i <= 100; ++i)
+	int i;
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0) printf("FizzBuzz ");
-		if (i % 3 == 0) printf("Fizz ");
-		if (i % 5 == 0) printf("Buzz ");
-		if (i * i * i * i % 15 == 1) printf("%d ", i);
+		if (!(i % 15))
+			printf ("FizzBuzz ");
+		else if (!(i % 3))
+			printf ("Fizz ");
+		else if (!(i % 5))
+			printf ("Buzz ");
+		else
+			printf ("%d ", i);
 	}
+	return 0;
 }
