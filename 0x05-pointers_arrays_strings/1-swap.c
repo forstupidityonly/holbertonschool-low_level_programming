df1f20ddd4bf83a806424f7b393b04d5a58211f6
Corbin <forstupidityonly@gmail.com>
2020-09-29 17:44:57 -0500

    i love pointers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,13 @@
 #include "holberton.h"
 /**
-  *
+  * swap_int - a = b & b = a
+  * @a: to be b
+  * @b: to be a
+  * Return: no
   */
 void swap_int(int *a, int *b)
 {
-
+	int c = *a;
+	*a = *b;
+	*b = c;
 }
