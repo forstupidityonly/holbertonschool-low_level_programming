8f758b3abee664828bf6b237c7dd1ad88bfaaa4c
Corbin <forstupidityonly@gmail.com>
2020-09-29 17:50:22 -0500

    while

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,17 @@
 #include "holberton.h"
 /**
-  *
+  * _strlen - find the len of a string
+  * @*s: the pointer to the string
+  * Return: the length 
   */
 int _strlen(char *s)
 {
+	int len = 0;
 
+	while (*s)
+	{
+		len++;
+		s++;
+	}
+	return (len);
 }
