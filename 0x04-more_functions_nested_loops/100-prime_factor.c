dd5bfb2e7c5aa37e07fe1fe7c0c281ad81da9f50
Corbin <forstupidityonly@gmail.com>
2020-10-01 10:16:17 -0500

    initial push

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,14 @@
 #include "holberton.h"
 /**
-  * 
+  * main - entry point
+  * Return: 0
   */
+int main(void)
+{
+	unsigned int long i, j = 612852475143;
+
+	for (i = 3; i < 782849; i = i + 2)
+	{
+		
+	}
+}
