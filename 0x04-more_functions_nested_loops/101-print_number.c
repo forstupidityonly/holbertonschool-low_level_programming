57801fd29b7a1464eafe4cfebaebc71c431b543e
Corbin <forstupidityonly@gmail.com>
2020-10-01 17:10:52 -0500

    lol

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,5 +6,16 @@
   */
 void print_number(int n)
 {
+	unsigned int num;
 
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	else
+		num = n;
+	if (num / 10)
+		print_number(num / 10);
+	_putchar('0' + num % 10);
 }
