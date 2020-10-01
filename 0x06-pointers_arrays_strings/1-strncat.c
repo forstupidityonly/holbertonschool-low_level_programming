7cd5704d9ce297b08ed66c354bb2812941d224b6
Corbin <forstupidityonly@gmail.com>
2020-10-01 10:26:13 -0500

    if statements

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+  * _strncat - cats two strings
+  * @src: the src
+  * @dest: the dest
+  * @n: the num of bitess to use from src
+  */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *i;
+	char *j;
+	int cnt = 0;
+
+	for (i = dest; *i; i++)
+		;
+	for (j = src; *j; i++, j++, cnt++)
+	{
+		if (cnt < n)
+			*i = *j;
+		else
+			return (dest);
+	}
+	*i = '\0';
+	return (dest);	
+}
