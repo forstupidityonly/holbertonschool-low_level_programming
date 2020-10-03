a18f681b98b1ae600480606e28f9649f8f788470
Corbin <forstupidityonly@gmail.com>
2020-10-03 10:24:41 -0500

    print?

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,14 +1,31 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <math.h>
 /**
   * main - entry point
   * Return: 0
   */
 int main(void)
 {
-	unsigned int long i, j = 612852475143;
+	int long num = 612852475143;
+	int i;
+	int largest = -1;
 
-	for (i = 3; i < 782849; i = i + 2)
+	while (num % 2 ==0)
 	{
-		
+		largest = 2;
+		num = num / 2;
 	}
+	for (i = 3; i <= sqrt(612952475143); i = i + 2)
+	{
+		while (num % i == 0)
+		{
+			largest = i;
+			num = num / i;
+		}
+	}
+	if (num > 2)
+		largest = num;
+	printf("%d\n", largest);
+	return (0);
 }
