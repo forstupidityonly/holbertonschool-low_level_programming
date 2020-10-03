bf5bb642a1478f15b151d7cb75d9b65ebe62cb3f
Corbin <forstupidityonly@gmail.com>
2020-10-03 10:47:06 -0500

    old assignments

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+  * print_times_table - print times table
+  * @n: the int of times table
+  * Return: no
+  */
+void print_times_table(int n)
+{
+	int aws;
+	int i;
+	int j;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				aws = i * j;
+				if (j == 0)
+					_putchar('0');
+				else if (j != 0 && aws < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(aws + '0');
+				}
+				else if (aws < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(aws / 10 + '0');
+					_putchar(aws / 10 + '0');
+				}
+				else if (aws > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((aws / 10) / 10 + '0');
+					_putchar((aws / 10) % 10 + '0');
+					_putchar(aws % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
