756292733dd3cd2c39f1d759b5eb9b9772bb6299
Corbin <forstupidityonly@gmail.com>
2020-10-03 10:54:44 -0500

    betty

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,7 +13,9 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 		for (i = 0; i <= n; i++)
+		{
 			for (j = 0; j <= n; j++)
+			{
 				aws = i * j;
 				if (j == 0)
 					_putchar('0');
@@ -31,15 +33,17 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(aws / 10 + '0');
-					_putchar(aws / 10 + '0');
+					_putchar(aws % 10 + '0');
 				}
 				else if (aws > 99)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar((aws / 10) / 10 + '0');
+					_putchar(((aws / 10)) / 10 + '0');
 					_putchar((aws / 10) % 10 + '0');
 					_putchar(aws % 10 + '0');
 				}
+			}
 			_putchar('\n');
+		}
 }
