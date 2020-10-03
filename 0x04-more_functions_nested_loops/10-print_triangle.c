d2f163ccd9a301d304882ed04981147196d1a68c
Corbin <forstupidityonly@gmail.com>
2020-10-03 10:13:33 -0500

    old projects

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,5 +6,21 @@
   */
 void print_triangle(int size)
 {
+	int i;
+	int j;
+	int n;
 
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size; j > i; j--)
+				_putchar(' ');
+			for (n = 0; n < i; n++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else if (size <= 0)
+		_putchar('\n');
 }
