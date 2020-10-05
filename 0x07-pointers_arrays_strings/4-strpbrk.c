7d770e33095cf007bd697885ce4deb476138bd76
Corbin <forstupidityonly@gmail.com>
2020-10-05 14:28:43 -0500

    the first occerence

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+  * _strpbrk -searches a string for a set of bites
+  * @s: the string
+  * @accept: the bites
+  */
+char *_strpbrk(char *s, char *accept)
+{
+	char i, *ptr;
+
+	for (i = *s; i != 0; s++, i = *s)
+	{
+		for (ptr = accept; *ptr != 0; ptr++)
+		{
+			if (i == *ptr)
+				return (s);
+		}
+	}
+	return (0);
+}
