3eba4b89e192fe68c2b5e621b508cca73ea40706
Corbin <forstupidityonly@gmail.com>
2020-10-05 13:00:46 -0500

    rework

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,9 +7,13 @@
   */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	char *aws = dest;
 
-	for (i = 0; i <= n; i++)
-		dest[i] = src[i];
-	return (dest);
+	while (n-- > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	return (aws);
 }
