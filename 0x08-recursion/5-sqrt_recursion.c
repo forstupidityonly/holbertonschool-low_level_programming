4ea667308207a91a96de8b2d4208b90c011b6ded
Corbin <forstupidityonly@gmail.com>
2020-10-07 15:11:58 -0500

    betty?

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+  * helper_three - help sqrt
+  * @i: int
+  * @n: int
+  * Return: yes
+  */
+int helper_three (int i, int n)
+{
+	if ((i * i) == n)
+		return (i);
+	if ((i * i) < n)
+		return (helper_three (i + 1, n));
+	if ((i * i) > n)
+		return (-1);
+	return (i);
+}
+/**
+  * _sqrt_recursion - sqrt
+  * @n: the int
+  * Return: answer
+  */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	else if (n > 1)
+		return (helper_three (1, n));
+	return (-1);
+}
