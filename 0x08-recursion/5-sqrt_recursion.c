0f2db26f3a8c3edc465495192774bbbecb123deb
Corbin <forstupidityonly@gmail.com>
2020-10-07 15:13:19 -0500

    betty

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,12 +5,12 @@
   * @n: int
   * Return: yes
   */
-int helper_three (int i, int n)
+int helper_three(int i, int n)
 {
 	if ((i * i) == n)
 		return (i);
 	if ((i * i) < n)
-		return (helper_three (i + 1, n));
+		return (helper_three(i + 1, n));
 	if ((i * i) > n)
 		return (-1);
 	return (i);
@@ -29,6 +29,6 @@ int _sqrt_recursion(int n)
 	if (n == 1)
 		return (1);
 	else if (n > 1)
-		return (helper_three (1, n));
+		return (helper_three(1, n));
 	return (-1);
 }
