a19903a16e985ba36a337b9966b4b35f9ef24a7c
Corbin <forstupidityonly@gmail.com>
2020-10-07 15:31:07 -0500

    pushhh

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+  * helper_four - help prime
+  * @n: int
+  * @i: int
+  * Return: yes
+  */
+int helper_four(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	else
+		return (helper_four(n, i - 1));
+}
+/**
+  * is_print_number - check prime
+  * @n: the int
+  * Return: yes
+  */
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	return (helper_four(n, n - 1));
+}
+
