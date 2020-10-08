9fc4e2e9f6a58be571b2a98840f3c5c65102723b
Corbin <forstupidityonly@gmail.com>
2020-10-07 21:16:52 -0500

    got deleted?

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "holnerton.h"
+/**
+  * _strlen_recursion - strlen
+  * @s: string
+  * Return: yes
+  */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s == '\0')
+		return (0);
+	if (*s != '\0')
+	{
+		len++;
+		len += _strlen_recursion(++s);
+		return(len);
+	}
+	return(0);
+}
