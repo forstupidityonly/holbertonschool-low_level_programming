431707ccce606914bac528ab867c61c455ba02df
Corbin <forstupidityonly@gmail.com>
2020-10-08 11:33:51 -0500

    thank god for plds

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -2,7 +2,22 @@
 /**
   *
   */
-int wildcmp(char *s1, char *s2);
+int wildcmp(char *s1, char *s2)
 {
-	where do i start!?!?!
+	if (*s1 == *s2 && *s1 && *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (!*s1 && !*s2)
+		return (1);
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) != *s1 && *(s2 + 1) != '*')
+			return (wildcmp(s1 + 1, s2));
+		else if (*(s2 + 1) != *s1 && *(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (!wildcmp(s1, s2 + 1))
+			return (wildcmp(s1 + 1, s2));
+		if (wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
 }
