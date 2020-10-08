44392ee1e5729858ebb6cad0f01284aecaf44a52
Corbin <forstupidityonly@gmail.com>
2020-10-07 21:10:35 -0500

    overcomplicated it

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,44 +1,13 @@
 #include "holberton.h"
 /**
-  * _strlen - strlen
-  * @s: string
-  * Return: len
-  */
-int _strlen(char *s)
-{
-	if (s[0] != '\0')
-		return (1 + _strlen(s + 1));
-	else
-		return (0);
-}
-/**
-  * helper_one - helper to print rev
-  * @s: the string
-  * @len: the length
-  * Return: no
-  */
-void helper_one(char *s, int len)
-{
-	if (len > 0)
-	{
-		_putchar(s[len]);
-		len--;
-		helper_one(s, len);
-	}
-	else if (len == 0)
-		_putchar('\n');
-}
-/**
   * _print_rev_recursion - rev string
   * @s: the string
   * Return: no
   */
 void _print_rev_recursion(char *s)
 {
-	int i = 0;
-	char *c = s;
-
-	i = _strlen(c);
-	helper_one(s, i);
+	is (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
-
