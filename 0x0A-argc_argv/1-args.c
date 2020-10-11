fbbcea3a59cfe2f8f112df3f223c5769870455fa
Corbin <forstupidityonly@gmail.com>
2020-10-11 12:23:06 -0500

    attribute unused

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
 /**
   * main - entry point
+  * @argc: the # of arg
+  * @argv: nut array
   * Return: 0
   */
-int main(int argc, char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
-	printf("%d\n", argc);
-	argv = argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }
