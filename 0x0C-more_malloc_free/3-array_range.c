af48d5baf7a0f748621e23e0b8405aed6b18eee8
Corbin <forstupidityonly@gmail.com>
2020-10-15 14:16:25 -0500

    betty

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 /**                                                                              
- * array_range - this creates an array of integers                                                      
- * @min: this is minumum number of numbers                                                          
- * @max: this is the max number of                                                              
- * Return: is int                                                                       
+ * array_range - make array of integers                                                      
+ * @min: min                  
+ * @max: max                                                             
+ * Return: yes                                                                      
  */
 int *array_range(int min, int max)
 {
-    int *ptr, itr;
-    if (min > max)
-        return (NULL);
-    ptr = malloc(sizeof(int) * ((max - min) + 1));
-    if (ptr == NULL)
-        return (NULL);
-    for (itr = 0; min <= max; min++, itr++)
-        ptr[itr] = min;
-    return (ptr);
+	int *ptr, itr;
+	if (min > max)
+		return (NULL);
+	ptr = malloc(sizeof(int) * ((max - min) + 1));
+	if (ptr == NULL)
+		return (NULL);
+	for (itr = 0; min <= max; min++, itr++)
+		ptr[itr] = min;
+	return (ptr);
 }
