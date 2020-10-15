b3c42525e5fbaa794437e77e8b2a5336f8ed6d12
Corbin <forstupidityonly@gmail.com>
2020-10-15 14:02:24 -0500

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,5 +4,15 @@
   */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *ptr;
+	unsigned int i;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		*((char *)ptr + i) = 0;
+	return (ptr);
 }
