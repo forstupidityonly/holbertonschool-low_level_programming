efb9c4278b6789dee75813d47b7678fd52c379ff
Corbin <forstupidityonly@gmail.com>
2020-10-19 13:59:37 -0500

    puppy

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,54 @@
+#include "dog.h"
+/**
+  * _strdup - dup a string
+  * @str: the string
+  * Return: NULL if str = NULL
+  */
+char *_strdup(char *str)
+{
+	unsigned int i, j = 0;
+	char *dest;
+
+	if (str == NULL)
+		return (0);
+	for (i = 0; str[i]; i++)
+		;
+	i += 1;
+	dest = malloc(i * sizeof(char));
+	if (!dest)
+		return (NULL);
+	for (j = 0; str[j]; j++)
+		dest[j] = str[j];
+	dest[j] = str[j];
+	return (dest);
+
+}
+/**
+  *
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *puppy;
+
+	puppy = malloc(sizeof(dog_t));
+	if (!puppy)
+	{
+		free(puppy);
+		return (NULL);
+	}
+	puppy->name = _strdup(name);
+	if (!puppy->name)
+	{
+		free(puppy);
+		return (NULL);
+	}
+	puppy->owner = _strdup(owner);
+	if (!puppy->owner)
+	{
+		free(puppy->name);
+		free(puppy);
+		return (NULL);
+	}
+	puppy->age = age;
+	return (puppy);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,7 @@ typedef struct dog
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
