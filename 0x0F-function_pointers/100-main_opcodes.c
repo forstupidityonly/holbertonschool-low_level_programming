d611a1931033500187d7af287e32963b15a82aa0
Corbin <forstupidityonly@gmail.com>
2020-10-21 21:27:33 -0500

    maddie

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - entry point
+  * @ac: arg count
+  * @av: arg ptr
+  * Return: yes
+  */
+int main(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[i]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+}
