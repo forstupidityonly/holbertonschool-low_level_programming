e7f5795032bca596b86ed0effe19f3da1a690c19
Corbin <forstupidityonly@gmail.com>
2020-10-22 10:58:42 -0500

    headderissues

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,25 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+/**
+  * main - entry point
+  * @ac: int
+  * @av: array
+  * Return: yes
+  */
+int main(int ac, char **av)
+{
+	if (ac != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((*av[2] == '/' || *av[2] == '%') && atoi(av[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf ("%d\n", get_op_func(av[2])(atoi(av[1]), atoi(av[3])));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+  * op_add - add things
+  * @a: int
+  * @b: int
+  * Return: yes
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+  * op_sub - sub things
+  * @a: int
+  * @b: int
+  * Return: yes
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+  * op_mul - mul things
+  * @a: int
+  * @b: int
+  * Return: yes
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+  * op_div - div things
+  * @a: int
+  * @b: int
+  * Return: yes
+  */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+  * op_mod - mod things
+  * @a: int
+  * @b: int
+  * Return: yes
+  */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
