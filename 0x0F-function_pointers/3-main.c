20f76d49b30db4e35b1d8622beee288d001ff46c
Corbin <forstupidityonly@gmail.com>
2020-10-22 11:09:35 -0500

    fixed

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,6 +20,6 @@ int main(int ac, char **av)
 		printf("Error\n");
 		exit(100);
 	}
-	printf ("%d\n", get_op_func(av[2])(atoi(av[1]), atoi(av[3])));
+	printf("%d\n", get_op_func(av[2]) (atoi(av[1]), atoi(av[3])));
 	return (0);
 }
