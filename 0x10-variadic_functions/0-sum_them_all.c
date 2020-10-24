83ff52038693d4f368c285c62dd239d7d0e5e562
Corbin <forstupidityonly@gmail.com>
2020-10-24 17:24:30 -0500

    negitive numbs

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,13 +10,12 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
-	unsigned int i, sum;
-
-	va_start(args, n);
+	unsigned int i;
+	int sum;
 
 	if (n == 0)
 		return (0);
-
+	va_start(args, n);
 	sum = 0;
 	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
