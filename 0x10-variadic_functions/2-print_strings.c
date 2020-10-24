53447b9b31a2f70ebaf338eabc0407589a3f6e72
Corbin <forstupidityonly@gmail.com>
2020-10-24 14:49:28 -0500

    test

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,4 +1,7 @@
 #include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
 /**
   * print_strings - prints string
   * @separator: string to go between strings
@@ -17,11 +20,15 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		now = va_arg(name, char *);
 		if (now != NULL)
+		{
 			printf("%s", now);
+		}
 		else
 			printf("(nil)");
 		if (i < n - 1 && separator != NULL)
+		{
 			printf("%s", separator);
+		}
 	}
 	printf("\n");
 	va_end(name);
