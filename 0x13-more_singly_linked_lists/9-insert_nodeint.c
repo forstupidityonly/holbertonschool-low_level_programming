f4199b7b76ac976614d2691b47bf6074e7e6095f
Corbin <forstupidityonly@gmail.com>
2020-11-04 14:15:17 -0600

    why was that so hard?

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+  * insert_nodeint_at_index - comeon
+  * @head: head
+  * @idx: index
+  * @n: new
+  * Return: yes
+  */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *buff;
+	listint_t *new_node;
+	unsigned int i;
+	
+	if (!head)
+		return (NULL);
+	buff = *head;
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n =n;
+	if (idx == 0)
+	{
+		new_node->next = buff;
+		*head = new_node;
+		return (*head);
+	}
+	for (i = 0; i < (idx - 1); i++)
+	{
+		if (buff)
+			buff = buff->next;
+		else
+			return (NULL);
+	}
+	new_node->next = buff->next;
+	buff->next = new_node;
+	return (new_node);
+}
