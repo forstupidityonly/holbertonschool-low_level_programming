db059e6c73f716427b49a29ea1d707349c733741
Corbin <forstupidityonly@gmail.com>
2020-11-04 14:17:04 -0600

    betty

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,14 +11,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *buff;
 	listint_t *new_node;
 	unsigned int i;
-	
+
 	if (!head)
 		return (NULL);
 	buff = *head;
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
 		return (NULL);
-	new_node->n =n;
+	new_node->n = n;
 	if (idx == 0)
 	{
 		new_node->next = buff;
