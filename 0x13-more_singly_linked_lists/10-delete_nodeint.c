c62ffe229c23f76fd0e3a893cc199b3c5cb61868
Corbin <forstupidityonly@gmail.com>
2020-11-04 14:28:18 -0600

    text

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+  * delete_nodeint_at_index - what u think
+  * @head: head
+  * @index: delete node
+  * Return: 1 -1
+  */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *spot;
+	listint_t *buff = NULL;
+
+	if (!(head && *head))
+		return (-1);
+	spot = *head;
+	if (index == 0)
+	{
+		buff = spot;
+		*head = spot->next;
+		free(buff);
+		return (1)
+	}
+	else
+		for (i = 0; i < (index - 1); i++)
+		{
+			if (spot->next)
+				spot = spot->next;
+			else
+				return (-1);
+		}
+	buff = spot->next;
+	spot->next = buff->next;
+	free(buff);
+	return (1)'
+}
