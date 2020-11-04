2a6540505660b743e2ac0e59ac72187d887d4ab6
Corbin <forstupidityonly@gmail.com>
2020-11-04 13:11:26 -0600

    betty

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
-  * pop_list - delete head
+  * pop_listint - delete head
   * @head: head
   * Return: info
   */
@@ -9,7 +9,7 @@ int pop_listint(listint_t **head)
 	listint_t *buff;
 	int info = 0;
 
-	if(!(*head))
+	if (!(*head))
 		return (0);
 	info += (*head)->n;
 	buff = *head;
