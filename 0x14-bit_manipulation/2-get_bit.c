d30692a48912d5b0a7211e077a7be2a994c08175
Corbin <forstupidityonly@gmail.com>
2020-11-05 13:32:01 -0600

    test

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+  * get_bit - git bit at index
+  * @n: int
+  * @index: ind
+  * Return: 0
+  */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (n == 0 && index < 64)
+		return (0);
+	n = n >> index;
+	return (n & 1);
+}
