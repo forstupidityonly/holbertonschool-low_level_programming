89a5e7be98099a711ea384f9319864aa38f28377
Corbin <forstupidityonly@gmail.com>
2020-12-14 10:13:59 -0600

    i like the c compiler

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+  *
+  */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+dlistint_t *add = malloc(sizeof(dlistint_t));
+dlistint_t *now;
+
+if (!head || !add)
+    return (NULL);
+add->n = n;
+add->next = NULL;
+if (*head == NULL)
+{
+add->prev = NULL;
+*head = add;
+}
+else
+{
+now = *head;
+while (now->next)
+{
+now = now->next;
+}
+now->next = add;
+add->prev = now;
+}
+return (add);
+}
