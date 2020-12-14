f86603c5f4bcb68de3144ad2816f3c01b0bac223
Corbin <forstupidityonly@gmail.com>
2020-12-14 14:36:00 -0600

    betty god damn

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,36 +7,36 @@
   */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current;
-    dlistint_t *phree;
+dlistint_t *current;
+dlistint_t *phree;
 
-    if (head)
-    {
-        current = *head;
-        if (index == 0)
-        {
-            if (!current)
-                return (-1);
-            *head = current->next;
-            if (*head)
-                (*head)->prev = NULL;
-            free(current);
-            return (1);
-        }
-        for (; current; current = current->next, index--)
-        {
-            if (index == 1)
-            {
-                phree = current->next;
-                if (!phree)
-                    break;
-                current->next = phree->next;
-                if (phree->next)
-                    phree->next->prev = current;
-                free(phree);
-                return (1);
-            }
-        }
-    }
-    return (-1);
+if (head)
+{
+current = *head;
+if (index == 0)
+{
+if (!current)
+return (-1);
+*head = current->next;
+if (*head)
+(*head)->prev = NULL;
+free(current);
+return (1);
+}
+for (; current; current = current->next, index--)
+{
+if (index == 1)
+{
+phree = current->next;
+if (!phree)
+break;
+current->next = phree->next;
+if (phree->next)
+phree->next->prev = current;
+free(phree);
+return (1);
+}
+}
+}
+return (-1);
 }
