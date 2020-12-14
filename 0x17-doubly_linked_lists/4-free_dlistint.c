a84db03bd97abdc6e79eb00d1a38bb6e472c9e3e
Corbin <forstupidityonly@gmail.com>
2020-12-14 10:30:57 -0600

    all the same free

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.c"
+/**
+  * free_dlistint - free
+  * @head: head
+  * Return: no
+  */
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *elements;
+
+while (head)
+{
+elements = head;
+head = head->next;
+free(elements);
+}
+}
