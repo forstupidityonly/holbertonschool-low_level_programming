abfef501c24acc86b69ae2475480ee5cc18e570e
Corbin <forstupidityonly@gmail.com>
2021-01-15 13:12:22 -0600

    betty documentation

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,6 +1,9 @@
 #include "hash_tables.h"
 /**
- * 
+ * key_index - returns the index in size that key would be in
+ * @key: searching for
+ * @size: size of the array
+ * Return: the index
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
