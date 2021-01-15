92cc94d33896e797059e1ebcd798b87b5fc7302e
Corbin <forstupidityonly@gmail.com>
2021-01-15 11:10:51 -0600

    betty pretty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,18 +1,20 @@
 #include "hash_tables.h"
 /**
- * 
+ * hash_table_create - create hash table
+ * @size: size of table
+ * Return: the table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	/*decelation of table*/
 	hash_table_t *table;
-	
+
 	/*bogus size*/
-	if(!size)
+	if (!size)
 		return (NULL);
 	table = malloc(sizeof(hash_table_t));
 	/*malloc fail*/
-	if(!table)
+	if (!table)
 		return (NULL);
 	table->size = size;
 	table->array = calloc(size, sizeof(hash_node_t *));
