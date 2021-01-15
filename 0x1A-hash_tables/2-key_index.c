a512cc442b57ac1943579171c1e85f8e33583784
Corbin <forstupidityonly@gmail.com>
2021-01-15 13:13:58 -0600

    betty tabs

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,7 +7,7 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    if(!size)
-        return(0);
-    return (hash_djb2(key) % size);
+	if (!size)
+		return (0);
+	return (hash_djb2(key) % size);
 }
