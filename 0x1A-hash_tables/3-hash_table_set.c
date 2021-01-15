5a78b558d099e722bb5b0e968107fb0a7350b3b5
Corbin <forstupidityonly@gmail.com>
2021-01-15 11:24:39 -0600

    betty headder

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+/**
+ * 
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+    hash_node_t *new = NULL, *head = NULL;
+    unsigned long int hash_value = 0;
+
+    if(!ht || !key || || !*key || !value)
+        return(0);
+    itr = key_index((unsigned char *)key, ht->size);
+    new = new_
+    /**/
+}
