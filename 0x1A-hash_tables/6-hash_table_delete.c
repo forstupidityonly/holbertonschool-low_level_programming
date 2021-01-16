c1bd9581c96f1c0461ca4f05bfe4c698a5d758d3
Corbin <forstupidityonly@gmail.com>
2021-01-15 21:24:50 -0600

    what the fuckkkkk

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - deletes the table
+ * @ht: the table
+ * Return: no
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *head = NULL;
+	unsigned long int i;
+
+	if (!ht)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		head = ht->array[i];
+		while (head)
+		{
+			ht->array[i] = ht->array[i]->next;
+			free(head->key);
+			free(head->value);
+			free(head);
+			head = ht->array[i];
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
